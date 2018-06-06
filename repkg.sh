db3b4f7ec234172d1881a535e69912ab75120854
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-06-06 18:21:06 +0200

    remove accidental debug code
    Thanks to @nikita-moor for noticing

--- repkg.sh
+++ repkg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-/home/sky/Programming/QtProjects/build-repkg-System_Qt_5_11_0-Debug/repkg update --stdin
+/usr/bin/repkg update --stdin
 
 updatePkg=$(/usr/bin/repkg list)
 if [ -n "$updatePkg" ]; then
